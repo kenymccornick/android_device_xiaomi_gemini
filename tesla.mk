15a9a7c718e793efc3d2cfabb556368e00760245
Nestor Gomez <kenymccornick1985@gmail.com>
2017-01-01 14:10:15 +0800

    gemini: Tesla-fy

--- tesla.mk
+++ tesla.mk
@@ -21,11 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gemini device
 $(call inherit-product, device/xiaomi/gemini/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Tesla stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/caf_fw.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cm_gemini
+PRODUCT_NAME := tesla_gemini
 PRODUCT_DEVICE := gemini
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5
